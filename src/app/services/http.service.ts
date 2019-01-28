c7db096a6b111470d45d921aa501884373c5771a
zhuying6666 <905003593@qq.com>
2019-01-28 10:42:23 +0800

    添加http-service

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -139,16 +139,12 @@ export class HttpService {
 
   public handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
     } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
       console.error(
         `Backend returned code ${error.status}, ` +
         `body was: ${error.error}`);
     }
-    // return an observable with a user-facing error message
     return throwError(
       'Something bad happened; please try again later.');
   };
@@ -159,7 +155,7 @@ export class HttpService {
   */
   asyncGetToken(usersName :string , usersPsw : string) : Observable < any >{
     return this.request({
-      method: 'get',
+      method: 'post',
       url: 'api/dashHeros'+usersName+usersPsw,
       data :{
         Name : usersName,
@@ -173,7 +169,7 @@ export class HttpService {
   */
   asyncRefreshToken(refreshToken: any,usersName :string , usersPsw : string) : Observable < any >{
     return this.request({
-      method: 'get',
+      method: 'post',
       url: 'api/dashHeros'+usersName+usersPsw+refreshToken,
       data: {
         refresh:refreshToken
